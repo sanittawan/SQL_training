009b66bfa5121833a409253ade2f63fd27ce67e1
Sanittawan Tan <sanittawan.t@gmail.com>
2021-04-10 14:22:10 +0700

    update

--- select_query.sql
+++ select_query.sql
@@ -1,18 +1,51 @@
 /* Select single column */
 SELECT
     name
-FROM movies
+FROM movies;
 
 /* Select multiple columns */
 SELECT
     rank,
     name,
     gross
+FROM movies;
+
+/* Select star */
+SELECT * 
+FROM movies;
+
+/* Aliasing a column */
+SELECT 
+    name as movie_name
+FROM movies;
+
+/* Arithmetic in select statement */
+SELECT (4+3) / 9; -- got 0.778
+
+/* Add a new column with a default value and alias a column name
+with this pattern
+SELECT
+ <column name>,
+ <expression> AS <alias>,
+ ...
+FROM <table name> 
+*/
+SELECT
+    name,
+    'TH' AS language,
+FROM movies;
+
+/* Limit numbers of shown results */
+SELECT *
 FROM movies
+LIMIT 5;
+
+/* Select distinct values in a column */
+SELECT DISTINCT studio
+FROM movies;
 
 /* */
 
-/* */
 
 /* */
 
